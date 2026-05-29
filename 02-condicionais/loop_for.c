b8cb790db32d6c914c112fdad132e80c77fbbe78
klinger-sabino <klingersabino07@gmail.com>
2026-05-28 22:25:36 -0300

    loop-for: numeros pares de 0 a 100

---
+++ 02-condicionais/loop_for.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    int i;
+
+    printf("=== Numeros pares de 1 a 100 ===\n");
+
+    for (i = 1; i <= 100; i++) {
+        if (i % 2 == 0) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
